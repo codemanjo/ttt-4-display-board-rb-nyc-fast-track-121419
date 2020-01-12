c6ecdb21c7cc53c4f35ee8c405d8b1eb66dd548d
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2020-01-12 01:41:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,16 @@
 
 
+board = ["x","x"," O","O","O"," x","O"," "]
 
 def display_board(board)
-board = (" "," "," "," "," "," "," "," ")
+#board = (" "," "," "," "," "," "," "," ")
 
-  puts "   |   |   "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+  puts " #{board[0]}  |#{board[1]}  | #{board[2]}  "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
   puts "-----------"
-  puts "   |   |   "
+  puts "  #{board[3]} | #{board[4]} |  #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts "  #{board[6]} | #{board[7]} |  #{board[8]} "
 end
 # Define display_board that accepts a board and prints
 # out the current state.
+display_board(board)
